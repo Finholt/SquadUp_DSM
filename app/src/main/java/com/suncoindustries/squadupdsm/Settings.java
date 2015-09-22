16186977252e89f095a3ca3001b031a8f6471f31
Connor Finholt <connor.finholt@gmail.com>
2015-09-22 17:48:32 -0500

    Finished Navigation Between Pages.

--- app/src/main/java/com/suncoindustries/squadupdsm/Settings.java
+++ app/src/main/java/com/suncoindustries/squadupdsm/Settings.java
@@ -1,9 +1,11 @@
 package com.suncoindustries.squadupdsm;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
 
 public class Settings extends AppCompatActivity {
 
@@ -11,6 +13,12 @@ public class Settings extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_settings);
+    }
+
+    public void toQuizProcedure(View view)
+    {
+        Intent intent = new Intent(this, InterestQuiz.class);
+        startActivity(intent);
     }
 
     @Override
