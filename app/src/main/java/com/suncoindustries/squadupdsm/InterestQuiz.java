af04a7d5a8ee563d8b2eb352e0c3652100a8c2cd
Derek C <derek.charles@drake.edu>
2015-09-22 19:28:00 -0500

    quiz2page

--- app/src/main/java/com/suncoindustries/squadupdsm/InterestQuiz.java
+++ app/src/main/java/com/suncoindustries/squadupdsm/InterestQuiz.java
@@ -1,13 +1,14 @@
 package com.suncoindustries.squadupdsm;
 
 import android.content.Intent;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
-import android.widget.EditText;
+import android.widget.ArrayAdapter;
 import android.widget.RadioButton;
+import android.widget.Spinner;
 
 public class InterestQuiz extends AppCompatActivity {
 
@@ -19,21 +20,19 @@ public class InterestQuiz extends AppCompatActivity {
         setContentView(R.layout.activity_interest_quiz);
     }
 
+    Spinner spinner = (Spinner) findViewById(R.id.spinner);
+    // Create an ArrayAdapter using the string array and a default spinner layout
+    ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this,
+            R.array.pronouns, android.R.layout.simple_spinner_item);
+// Specify the layout to use when the list of choices appears
+    adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+// Apply the adapter to the spinner
+    spinner.setAdapter(adapter);
+
+
     public void onRadioButtonClicked(View view) {
         boolean checked = ((RadioButton) view).isChecked();
 
-        switch (view.getId()){
-            case R.id.he:
-                if(checked)
-                    //
-                break;
-            case R.id.she:
-                //
-                break;
-            case R.id.them:
-                //
-                break;
-        }
         // Check which radio button was clicked
         switch (view.getId()) {
             case R.id.radio_superman:
